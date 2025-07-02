ab76ebf3fed941f94fcae208588aa037343bbb50
JanSeliv <janseliw@gmail.com>
2025-07-01 23:59:44 -0400

    [UE5.6][Warning] Fixed 'ModuleRules.bEnableNonInlinedGenCppWarnings' is obsolete: 'Deprecated in UE5.6 - Replace with ModuleRules.CppCompileWarningSettings.NonInlinedGenCppWarningLevel'

--- Source/SettingsWidgetConstructor/SettingsWidgetConstructor.Build.cs
+++ Source/SettingsWidgetConstructor/SettingsWidgetConstructor.Build.cs
@@ -8,7 +8,7 @@ public class SettingsWidgetConstructor : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 		CppStandard = CppStandardVersion.Latest;
-		bEnableNonInlinedGenCppWarnings = true;
+		CppCompileWarningSettings.NonInlinedGenCppWarningLevel = WarningLevel.Error;
         
 		PublicDependencyModuleNames.AddRange(new[]
 			{
