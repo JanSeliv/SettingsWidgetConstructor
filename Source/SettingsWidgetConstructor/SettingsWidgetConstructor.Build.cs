1450b372a6224869b1b3261732fc40f7b5b1008d
JanSeliv <janseliw@gmail.com>
2025-11-28 22:05:40 -0500

    [Bug][UE5.7] Fixed compilation error after engine update on MacOS: do not force cppstandard, but use the platform default version

--- Source/SettingsWidgetConstructor/SettingsWidgetConstructor.Build.cs
+++ Source/SettingsWidgetConstructor/SettingsWidgetConstructor.Build.cs
@@ -7,7 +7,6 @@ public class SettingsWidgetConstructor : ModuleRules
 	public SettingsWidgetConstructor(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		CppStandard = CppStandardVersion.Latest;
 		CppCompileWarningSettings.NonInlinedGenCppWarningLevel = WarningLevel.Error;
         
 		PublicDependencyModuleNames.AddRange(new[]
